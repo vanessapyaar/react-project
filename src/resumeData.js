5492a60637ccbb9dc3972a2d1030df6feabd72e9
= <=>
2020-12-08 19:37:52 -0500

    Project Started

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"https://www.linkedin.com/feed/",
+    "name": "Vanessa Castilho",
+    "role": "UX Developer",
+    "linkedinId":"vanessa-castilho-decarvalho",
+    "skypeid": "Your skypeid",
+    "roleDescription": "My current role involves a combination of product design and development. I like solving problems with the final user in mind. I'm passionate about learning new technologies. On my free time I enjoy mountain biking, bird watching and scuba diving.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/vanessa-castilho-decarvalho/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/vanessapyaar",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://twitter.com/lunar__v",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a Frontend Dev and Designer at Boats Group. For the past 3 years, I have practiced my problem-solving skills, data analysis, design chops, and coding. I study and gather requirements to solve low engagement/low performance frontend problems with a user story in mind. My role is mixed and it keeps me challenged to never stop learning. I'm passionate about what I do but very detached from my outcomes, therefore I handle criticism as a normal part of any creative work. Professional yet friendly, I have an easy time walking through diversity and establishing connections. Making friends along the way is a plus that I am grateful for.",
+    "address":"Florida",
+    "website":"https://github.com/vanessapyaar",
+    "education":[
+      {
+        "UniversityName":"IBM",
+        "specialization":"Enterprise Design Thinking - 20h ",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2020",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"University of Michigan",
+        "specialization":"Responsive Design - 32h",
+        "MonthOfPassing":"Mar",
+        "YearOfPassing":"2020",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Boats Group LLC",
+        "specialization":"Websites Specialist",
+        "MonthOfLeaving":"May",
+        "YearOfLeaving":"2019",
+        "Achievements":"Managed and implemented +40 fully responsive websites with unique business requirements and content. Designed new components for the Website product improving the UX of templates."
+      },
+      {
+        "CompanyName":"UX Developer",
+        "specialization":"Technology Department",
+        "MonthOfLeaving":"May 2020",
+        "YearOfLeaving":"to Present",
+        "Achievements":"I am proud of my 2020 accomplishments, being able to design, build complex A/B tests, and also deploy code to the frontend. Designed and tested a Homepage iteration for boats.com with the search form above the fold. The variation increased page views and boat details page views by 2%. I also deployed the winner variation to the front-end, speeding up the development process, and reducing tech debt."
+      }
+    ],
+    "skillsDescription":"My skills range from design to code, A/B testing and prototyping.",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"It was a pleasure to work with Vanessa for more than two years, she is an awesome person, always so kind, funny, and easy to work with, and an amazing professional who is always wanting to improve and learn new things.",
+        "name":"Ricard Panades"
+      },
+      {
+        "description":"I knew about Vanessa before we even worked on the same team. I heard so many great things about her and her ambitions. She’s always open to feedback and trying to solve the right problems. It’s more than what someone feels like the solution is but listening to the users/customers and understanding their needs. ",
+        "name":"Candi Lemoine"
+      }
+    ]
+  }
+  
+  export default resumeData
